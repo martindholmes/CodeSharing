62d2dd198646b913c0fa22a0a63b0ac6ebaf4919
martindholmes <martindholmes@e900da7f-0833-47e1-bfb2-03406dd6c718>
2014-10-12 16:45:46 +0000

    Tweaks to build script.

--- build_release_package.sh
+++ build_release_package.sh
@@ -8,8 +8,8 @@ echo "*** Creating a release package for the sample implementation of CodeSharin
 echo ""
 rm -rf tmp
 mkdir tmp
-REVISION=`svnversion trunk | sed 's/^[0-9]*://p'`
-echo "Trunk revision is : $REVISION"
+REVISION=`svnversion -n trunk | sed -r 's/^[0-9]+://'`
+echo "Trunk revision is : ${REVISION}"
 echo ""
 echo "CodeSharing build from SVN revision $REVISION" >> tmp/README.txt
 echo "Packaged on $(date)." >> tmp/README.txt
@@ -21,3 +21,4 @@ cp tmp/codesharing.zip codesharing_rev_${REVISION}.zip
 rm -rf tmp
 echo ""
 echo "*** Created codesharing_rev_$REVISION.zip file ***"
+echo ""
