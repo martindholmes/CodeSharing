030b46f5ecb7a6c2cf8d7b7db69ee3d152f68ac4
martindholmes <mholmes@uvic.ca>
2015-08-20 16:00:21 -0700

    Reworking the build script a bit.

--- build_release_package.sh
+++ build_release_package.sh
@@ -6,26 +6,26 @@
 echo ""
 echo "*** Creating a release package for the sample implementation of CodeSharing. ***"
 echo ""
-CHANGES=`svn status trunk | grep [AMCDG]`
-if [ -n "$CHANGES" ]; then
-    echo "You have uncommitted changes in trunk."
+CHANGES=
+if [[ -n $(git diff --exit-code) ]]; then
+    echo "You have uncommitted changes in the repo."
     echo "You should probably commit them before making a package."
     echo "Press Control + C to exit this process and commit your changes."
     read
 fi
 rm -rf tmp
 mkdir tmp
-REVISION=`svnversion -n trunk | sed -r 's/^[0-9]+://'`
-echo "Trunk revision is : ${REVISION}"
+HASH=`git log --pretty=format:'%h' -n 1`
+echo "Git revision is : ${HASH}"
 echo ""
-echo "CodeSharing build from SVN revision $REVISION" >> tmp/README.txt
+echo "CodeSharing build from Git revision $HASH" >> tmp/README.txt
 echo "Packaged on $(date)." >> tmp/README.txt
 echo "" >> tmp/README.txt
-cat trunk/instructions.txt >> tmp/README.txt
-zip -rj tmp/codesharing.zip trunk/*
+cat code/instructions.txt >> tmp/README.txt
+zip -rj tmp/codesharing.zip code/*
 zip -rj tmp/codesharing.zip tmp/README.txt
-cp tmp/codesharing.zip codesharing_rev_${REVISION}.zip
+cp tmp/codesharing.zip codesharing_rev_${HASH}.zip
 rm -rf tmp
 echo ""
-echo "*** Created codesharing_rev_$REVISION.zip file ***"
+echo "*** Created codesharing_rev_$HASH.zip file ***"
 echo ""
